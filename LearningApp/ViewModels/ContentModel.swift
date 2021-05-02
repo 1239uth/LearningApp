4289c6984b6a246c55a5beb467ce0f8f078985d0
Uthman Mohamed <83053931+1239uth@users.noreply.github.com>
2021-05-02 00:41:44 -0400

    add module to models and parse json data

--- LearningApp/ViewModels/ContentModel.swift
+++ LearningApp/ViewModels/ContentModel.swift
@@ -9,4 +9,49 @@ import Foundation
 
 class ContentModel: ObservableObject {
     
+    @Published var modules = [Module]()
+    
+    var styleData: Data?
+    
+    init () {
+        
+        getLocalData()
+        
+    }
+    
+    func getLocalData() {
+        
+        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        //guard jsonUrl != nil else { return }
+        
+        do {
+            
+            let jsonData = try Data(contentsOf: jsonUrl!)
+            
+            let jsonDecoder = JSONDecoder()
+            
+            let myModules = try jsonDecoder.decode([Module].self, from: jsonData)
+            
+            self.modules = myModules
+            
+        } catch {
+            print(error)
+        }
+        
+        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
+        
+        guard styleUrl != nil else {return}
+        
+        do {
+            
+            let styleData = try Data(contentsOf: styleUrl!)
+            
+            self.styleData = styleData
+        } catch {
+            print(error)
+        }
+        
+    }
+    
 }
