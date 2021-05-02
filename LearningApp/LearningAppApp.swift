574805ae1bf428825b43447c4afe08d4651475c8
Uthman Mohamed <83053931+1239uth@users.noreply.github.com>
2021-05-01 23:44:51 -0400

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Uthman Mohamed on 2021-05-01.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
